4727e95b7bdf6e603590327bdf02c7c0d16ca724
Polina Savateeva <polina.savateeva@geogracom.com>
2026-03-23 01:04:35 +0300

    fix: правит runtime ошибку

--- src/entities/sidebaruicontent/model/constants.ts
+++ src/entities/sidebaruicontent/model/constants.ts
@@ -2,7 +2,7 @@ import { lazy } from "react"
 
 const SubContent = lazy(() => import("../../subwaycontent/subwaycontent"))
 const BusContent = lazy(() => import("../../buscontent/buscontent"))
-const DistrictContent = lazy(() => import("../../districtscontent/districtscontent"))
+const DistrictContent = lazy(() => import("../../districtcontent/districtcontent"))
 const StreetsContent = lazy(() => import("../../streetcontent/streetcontent"))
 const CustomContent = lazy(() => import("../../customcontent/customcontent"))
 
