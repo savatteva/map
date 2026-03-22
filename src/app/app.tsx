a9ed7b5f039fd19c8e8a370a71c6aa891ac15eed
Polina Savateeva <polina.savateeva@geogracom.com>
2026-03-23 00:40:42 +0300

    update: добавляет файлы

---
+++ src/app/app.tsx
@@ -0,0 +1,18 @@
+import { Routes, Route } from 'react-router-dom';
+import 'maplibre-gl/dist/maplibre-gl.css';
+import { MainPage } from '@/pages';
+import { QueryProvider } from './providers';
+import "../shared/styles/variables.css"
+import "../shared/assets/fonts.css"
+
+function App() {
+  return (
+    <QueryProvider>
+      <Routes>
+        <Route path='/' element={<MainPage />}/>
+      </Routes>
+    </QueryProvider>
+  )
+}
+
+export default App
